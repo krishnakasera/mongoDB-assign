a8936f65f8be05765f05cdaea1d31b5b1250afad
krishnakasera <kishan8114270058@gmail.com>
2026-03-30 09:48:51 +0530

    Remove checkmark from DB connection log

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ app.use("/api/users", userRoutes);
 // DB connect + start server
 mongoose.connect("mongodb://kishan8114270058_db_user:krishna123@ac-hla54ix-shard-00-00.z3hmwuu.mongodb.net:27017,ac-hla54ix-shard-00-01.z3hmwuu.mongodb.net:27017,ac-hla54ix-shard-00-02.z3hmwuu.mongodb.net:27017/myDB?ssl=true&replicaSet=atlas-11nymg-shard-0&authSource=admin&appName=Cluster0")
 .then(() => {
-  console.log("✅ DB Connected");
+  console.log(" DB Connected");
 
   app.listen(3000, () => {
     console.log(" Server running on port 3000");
