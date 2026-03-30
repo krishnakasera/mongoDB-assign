0fdf6b54c99f925905b86297b8c55bcf0c40e20f
krishnakasera <kishan8114270058@gmail.com>
2026-03-30 09:53:52 +0530

    Update server.js

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const userRoutes = require("./routes/userRoutes");
 app.use("/api/users", userRoutes);
 
 // DB connect + start server
-mongoose.connect("mongodb://kishan8114270058_db_user:krishna123@ac-hla54ix-shard-00-00.z3hmwuu.mongodb.net:27017,ac-hla54ix-shard-00-01.z3hmwuu.mongodb.net:27017,ac-hla54ix-shard-00-02.z3hmwuu.mongodb.net:27017/myDB?ssl=true&replicaSet=atlas-11nymg-shard-0&authSource=admin&appName=Cluster0")
+mongoose.connect("mongodb://kishan8114270058_db_user:kri*******@ac-hla54ix-shard-00-00.z3hmwuu.mongodb.net:27017,ac-hla54ix-shard-00-01.z3hmwuu.mongodb.net:27017,ac-hla54ix-shard-00-02.z3hmwuu.mongodb.net:27017/myDB?ssl=true&replicaSet=atlas-11nymg-shard-0&authSource=admin&appName=Cluster0")
 .then(() => {
   console.log(" DB Connected");
 
